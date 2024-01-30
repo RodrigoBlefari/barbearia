eed7c0d7e1067f3f9f81a9327d9fe321648c4720
Rodrigo Blefari Gonçalves <rodrigo.blefari@gmail.com>
2024-01-30 02:24:17 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,19 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 const Home = () => {
   return (
     <div>
-      <Header />;
+      <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Rodrigo</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE ',' dd 'de' MMMM ", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 };
